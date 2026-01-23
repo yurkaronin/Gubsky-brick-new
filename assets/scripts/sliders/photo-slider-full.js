159ec3289efd48e657a98689c48ae8fbf85684fd
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2026-01-23 14:54:27 +0300

    Слайдер с фотографиями на всю ширину

---
+++ assets/scripts/sliders/photo-slider-full.js
@@ -0,0 +1,24 @@
+// \assets\scripts\sliders\photo-slider-full.js
+document.addEventListener('DOMContentLoaded', function () {
+  const photoSliderFull = new Swiper('.photo-slider-full .swiper', {
+    spaceBetween: 1,
+    loop: true,
+    slidesPerView: 'auto',
+    initialSlide: 1, // Слайд №2 (индексация с 0)
+
+     breakpoints: {
+      320: {
+        centeredSlides: true,
+      },
+      1023: {
+        centeredSlides: true,
+      }
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.photo-slider-full .swiper-button-next',
+      prevEl: '.photo-slider-full .swiper-button-prev',
+    },
+  })
+});
