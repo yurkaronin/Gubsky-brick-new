21e345c1be0c81b8b81bb298cf58eb8d0493c04c
yurkaronin <47382042+yurkaronin@users.noreply.github.com>
2026-01-30 09:46:48 +0300

    Включил слайдер на главной и сделал запасной вариант секции без слайдера

---
+++ assets/scripts/sliders/home-hero-slider.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.home-hero-slider .swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+
+    // If we need pagination
+    pagination: {
+      el: '.home-hero .swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.home-hero .swiper-button-next',
+      prevEl: '.home-hero .swiper-button-prev',
+    },
+
+  });
+});
