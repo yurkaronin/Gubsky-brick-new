67256343e75687cc582db5aebecf4eef8682ba89
yurkaronin <47382042+yurkaronin@users.noreply.github.com>
2026-01-30 11:41:28 +0300

    доработка скрипта для слайдера на главной

--- assets/scripts/sliders/home-hero-slider.js
+++ assets/scripts/sliders/home-hero-slider.js
@@ -1,28 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.home-hero-slider .swiper', {
-    // Optional parameters
+  const subSwiper = new Swiper('.home-hero-sub-slider .swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 7,
+    watchSlidesProgress: true,
+    slideToClickedSlide: true,
+  });
+
+  const mainSwiper = new Swiper('.home-hero-slider .swiper', {
     loop: true,
     slidesPerView: 1,
-
-    // If we need pagination
     pagination: {
       el: '.home-hero .swiper-pagination',
     },
-
-    // Navigation arrows
     navigation: {
       nextEl: '.home-hero .swiper-button-next',
       prevEl: '.home-hero .swiper-button-prev',
     },
-
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.home-hero-sub-slider .swiper', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 7,
+    thumbs: {
+      swiper: subSwiper,
+    },
   });
 });
